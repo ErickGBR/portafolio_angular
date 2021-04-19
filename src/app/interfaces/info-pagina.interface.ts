e6b07478b68d3f73b6e9d81e100087d456192e54
Erick Burgos <eburgos@takhyon.com>
2021-04-19 01:02:07 -0600

    portafolio terminado

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,11 @@
+
+export interface InfoPagina{
+    titulo?:string,
+    email?:string,
+    nombre_corto?:string,
+    pagina_autor?:"https://erick-burgos.com",
+    facebook?:"https://facebook.com",
+    twitter?:"https://twitter.com",
+    instagram?:"https://www.instagram.com/erickburgos52/?hl=es"
+    equipo_trabajo?:[]
+}
